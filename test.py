b8142c429717567ea6eccb666eac2ca81d6fe0b0
devops324 <jovepo8386@edxplus.com>
2022-09-28 02:25:11 -0700

    initial commit

--- test.py
+++ test.py
@@ -6,4 +6,4 @@ s = (a + b + c)
 
 # calculate the area
 area = (s*(s-a)*(s-b)*(s-c)) ** 0.5
-print('The area of the triangle is with sides as (%2d, %2d, %2d) is %0.2f' %a %b %c %area)
+print(f'The area of the triangle is with sides as ({a}, {b}, {c}) is {area}')
